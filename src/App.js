3a38e84cfbb8d1af2fac226a21997f4e79ef732d
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-15 05:52:47 +0200

    Updated: src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar";
+// import Footer from "./components/Navbar";
+import Routes from "./components/Routes.js";
+
 function App() {
-  return <header>CLUB SITES</header>;
+  return (
+    <>
+      <Navbar></Navbar>
+      {/* <Footer></Footer> */}
+      <Routes></Routes>
+    </>
+  );
 }
 
 export default App;
