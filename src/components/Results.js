35a17e90a6b484bd1357671356a567ce8bdf33b2
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-16 06:24:54 +0200

    Updated: src/components/Results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactPlayer from "react-player";
 
+import { useResultContext } from "./ResultsContextProvider";
 const Results = () => {
+  const { results, isLoading, getResults, searchItem } = useResultContext();
+  const location = useLocation();
+  if (isLoading) return "Loading";
+
   return <div>Results</div>;
 };
 
