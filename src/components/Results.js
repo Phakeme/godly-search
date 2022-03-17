60d0100bb22933fa06b3946de6b8e27434cf48c4
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-17 06:20:12 +0200

    Updated: src/components/Results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,13 +2,30 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ReactPlayer from "react-player";
 
-import { useResultContext } from "./ResultsContextProvider";
+import { useResultContext } from "../Context/ResultsContextProvider";
+
 const Results = () => {
   const { results, isLoading, getResults, searchItem } = useResultContext();
   const location = useLocation();
+
+  useEffect(() => {
+    // getResults("/search/q=Javascript&num=30");
+  }, []);
+
   if (isLoading) return "Loading";
 
-  return <div>Results</div>;
+  switch (location.pathname) {
+    case "/search":
+      return "Search";
+    case "/images":
+      return "Images";
+    case "/news":
+      return "News";
+    case "/videos":
+      return "Videos";
+    default:
+      return "Error";
+  }
 };
 
 export default Results;
