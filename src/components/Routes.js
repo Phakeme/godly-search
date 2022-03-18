4e7e94091e6df68811d2d8b458cf1a17553daf4d
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-18 06:01:13 +0200

    Updated: src/components/Routes.js

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,14 +2,13 @@ import React from "react";
 import Results from "./Results";
 import Images from "./Images";
 import { Routes, Route } from "react-router-dom";
+import { Home } from "./Home";
 
 const NavRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route axact path="/" element={<Results />}>
-          {/* <Navigate to="/search" /> */}
-        </Route>
+        <Route axact path="/" element={<Home />}></Route>
         <Route axact path="/search" element={<Results />}></Route>
         <Route axact path="/images" element={<Images />}></Route>
       </Routes>
