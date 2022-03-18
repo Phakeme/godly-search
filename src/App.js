61bd66b565ef4753beffb6e6a23610f68675828d
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-18 05:58:55 +0200

    Updated: src/App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Navbar from "./components/Navbar";
-// import Footer from "./components/Navbar";
 import Routes from "./components/Routes.js";
+// import Footer from "./components/Navbar";
+import Container from "@mui/material/Container";
 
 function App() {
   return (
-    <>
+    <Container maxWidth="md">
       <Navbar></Navbar>
-      {/* <Footer></Footer> */}
       <Routes></Routes>
-    </>
+      {/* <Footer></Footer> */}
+    </Container>
   );
 }
 
