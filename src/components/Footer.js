8134a0bd116ad2b765fdfbcbafcde3b0a3df7154
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-19 08:34:39 +0200

    Updates: src/components/Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Container from "@material-ui/core/Container";
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div style={{ backgroundColor: "#f2f2f2", minHeight: 100 }}>
+      <span>Footer</span>
+    </div>
+  );
 };
 
 export default Footer;
