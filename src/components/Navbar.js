5dff8868a1d575026c9b87fe72a00aa3e1892c8d
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-19 08:35:29 +0200

    Updates: src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Toolbar from "@material-ui/core/Toolbar";
 
-const Navbar = () => {
+export default function ButtonAppBar() {
   return (
-    <div>
-      <header>
-        <Link to="/">Poogle</Link>
-        <Link to="/images">Images</Link>
-        <Link to="/search">Search</Link>
-      </header>
-    </div>
+    <Box>
+      <AppBar position="static" elevation={0}>
+        <Toolbar
+          style={{
+            display: "flex",
+            justifyContent: "right",
+          }}
+        >
+          Login
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-};
+}
 
-export default Navbar;
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const Navbar = () => {
+//   return (
+//     <header>
+//       <Link to="/">Poogle</Link>
+//       <Link to="/images">Images</Link>
+//       <Link to="/search">Search</Link>
+//     </header>
+//   );
+// };
+
+// export default Navbar;
