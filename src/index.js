7b263669f498ab09dd5014726e1e88248e69efbe
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-19 08:38:11 +0200

    Updates: src/index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,28 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { ResultsContextProvider } from "./Context/ResultsContextProvider";
+import createTheme from "@material-ui/core/styles/createTheme";
+import { ThemeProvider } from "@material-ui/core/styles";
+
 import reportWebVitals from "./reportWebVitals";
+
+const theme = createTheme({
+  palette: {
+    phakeme: {
+      main: "#blue",
+    },
+  },
+});
 
 ReactDOM.render(
   <ResultsContextProvider>
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <Router>
+          <App />
+        </Router>
+      </React.StrictMode>
+    </ThemeProvider>
   </ResultsContextProvider>,
   document.getElementById("root")
 );
