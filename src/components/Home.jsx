150dca81f5218a3a794211e0420d602a25ef3c49
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-25 17:43:25 +0200

    Updated: src/components/Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,25 +3,27 @@ import Logo from "../images/Google-Logo.wine.svg";
 
 const Home = () => {
   return (
-    <div className="flex flex-col bg-red-100">
-      <div>
-        <img width="272px" src={Logo} alt="" />
-      </div>
-      <div>
-        <div>
-          <input
-            placeholder="Search…"
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-            inputProps={{ "aria-label": "search" }}
-          />
+    <div className="flex justify-center items-center h-full">
+      <div className="bg-red-100 w-full">
+        <div className="flex justify-center">
+          <img width="272px" src={Logo} alt="" />
+        </div>
+        <div className="p-8">
+          <div>
+            <input
+              placeholder="Search…"
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <div>Feeling lucky</div>
+          <div>Lan links</div>
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </div>
       </div>
-      <div>Feeling lucky</div>
-      <div>Lan links</div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
