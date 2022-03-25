f79a2bd1e36c79c12662853791c70172f2c0becb
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-25 17:43:02 +0200

    Updates: src/Containers/HomeContainer.jsx

--- src/Containers/HomeContainer.jsx
+++ src/Containers/HomeContainer.jsx
@@ -5,15 +5,15 @@ import Footer from "../components/Footer";
 
 export const HomeContainer = () => {
   return (
-    <div className="container mx-auto h-screen bg-gray-500">
-      <div className="flex flex-col">
-        <div className="flex-none">
+    <div className="container mx-auto h-screen">
+      <div className="flex flex-col h-screen">
+        <div className="">
           <Navbar />
         </div>
         <div className="grow">
           <Home />
         </div>
-        <div className="flex-none">
+        <div className="bg-yellow-500 item-end">
           <Footer />
         </div>
       </div>
