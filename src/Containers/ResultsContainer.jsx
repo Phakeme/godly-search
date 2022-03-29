50b02d0345aee0c630b98c6012015b6a9dc58d20
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-29 05:38:43 +0200

    src/Containers/ResultsContainer.jsx

---
+++ src/Containers/ResultsContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import Home from "../components/Home";
+// import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export const ResultsContainer = () => {
+  return (
+    <div className="">
+      <span>Results</span>
+      <Footer></Footer>
+    </div>
+  );
+};
