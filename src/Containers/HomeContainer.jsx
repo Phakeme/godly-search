f190cf8d952061b79a8892c5d6ad18120c4724a0
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-30 05:53:18 +0200

    src/Containers/HomeContainer.jsx

--- src/Containers/HomeContainer.jsx
+++ src/Containers/HomeContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "../components/Home";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/NavbarHome";
 import Footer from "../components/Footer";
 
 export const HomeContainer = () => {
