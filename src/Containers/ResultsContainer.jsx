f7793fe4d7bb48b2ebb6cee657835aac243c9399
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-03-31 06:41:11 +0200

    src/Containers/ResultsContainer.jsx

--- src/Containers/ResultsContainer.jsx
+++ src/Containers/ResultsContainer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Results from "../components/Results";
 import Navbar from "../components/NavbarResults";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
+import { CustomContainer } from "../components/util/CustomContainer";
 
 export const ResultsContainer = () => {
   return (
     <div className="">
       <Navbar />
-      <Results />
-      <Footer></Footer>
+      <CustomContainer>
+        <Results />
+      </CustomContainer>
+      {/* <Footer></Footer> */}
     </div>
   );
 };
