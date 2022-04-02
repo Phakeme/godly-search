794174ed8083d8a2ef0b2a8255b79c6603e2e1ed
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-02 07:38:25 +0200

    src/components/NavbarHome.jsx

--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
-/*
---Separate Data from the rest of the Component
-*/
+import { EachLink } from "./Navigations/EachLink";
+
 export default function AppBar() {
-  // I created the info an array of objects and stored the label, path and type btn true or false
   const info = [
     { label: "Gmail", path: "/", btn: false },
     { label: "Images", path: "/", btn: false },
-    { label: "Gmail", path: "/", btn: true },
+    { label: "Sign in", path: "/", btn: true },
   ];
-
-  // Made EachLink component and render btn conditionally
-  const EachLink = ({ btn, path, label }) => {
-    return (
-      <Link to={path}>
-        {btn ? (
-          <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
-            {label}
-          </button>
-        ) : (
-          label
-        )}
-      </Link>
-    );
-  };
 
   return (
     <header className="container mx-auto ">
