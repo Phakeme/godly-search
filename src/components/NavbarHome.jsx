55b38b2cb8a7ff329badb0fd58d6ef5baf3a8601
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-02 06:22:50 +0200

    src/components/NavbarHome.jsx

--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+/*
+--Separate Data from the rest of the Component
+*/
 export default function AppBar() {
+  // I created the info an array of objects and stored the label, path and type btn true or false
+  const info = [
+    { label: "Gmail", path: "/", btn: false },
+    { label: "Images", path: "/", btn: false },
+    { label: "Gmail", path: "/", btn: true },
+  ];
+
+  // Made EachLink component and render btn conditionally
+  const EachLink = ({ btn, path, label }) => {
+    return (
+      <Link to={path}>
+        {btn ? (
+          <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+            {label}
+          </button>
+        ) : (
+          label
+        )}
+      </Link>
+    );
+  };
+
   return (
     <header className="container mx-auto ">
-      <nav>
-        <ul className="flex items-center justify-end h-12">
-          <li className="ml-2 pl-4 text-sm">
-            <Link to="/">Gmail</Link>
-          </li>
-          <li className="ml-2 pl-4 text-sm">
-            <Link to="/">Images</Link>
-          </li>
-          <li className="ml-2 pl-4 text-sm">
-            <Link to="/">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
-                Sign in
-              </button>
-            </Link>
-          </li>
+      <nav className="w-full">
+        <ul className="flex items-center lg:justify-end  justify-center h-12">
+          {info.map(({ btn, label, path }, index) => (
+            <li className="ml-2 pl-4 text-sm" key={index}>
+              <EachLink btn={btn} path={path} label={label} />
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
