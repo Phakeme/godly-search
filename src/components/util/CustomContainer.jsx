606b77b2ffd1d0de5f82d895fd12dbdfd3874fa2
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-04 05:57:45 +0200

    src/components/util/CustomContainer.jsx

--- src/components/util/CustomContainer.jsx
+++ src/components/util/CustomContainer.jsx
@@ -2,7 +2,5 @@ import React from "react";
 import "./style.css";
 
 export const CustomContainer = ({ children }) => {
-  return (
-    <div className="container mx-auto pl-0 xl:pl-36 h-full">{children}</div>
-  );
+  return <div className="container mx-auto pl-0 xl:pl-36">{children}</div>;
 };
