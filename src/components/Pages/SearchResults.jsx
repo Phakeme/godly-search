0b9ff69bf027d866d1cf4fc25874e459fc4bef20
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-09 11:52:01 +0200

    src/components/Pages/SearchResults.jsx

---
+++ src/components/Pages/SearchResults.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const SearchResults = ({ localState, searchTerm }) => {
+  return (
+    <div className="">
+      <p className="text-base mb-2 mt-2 text-gray-500">
+        About {localState?.results?.length} results {searchTerm}
+      </p>
+      {localState && (
+        <div className="">
+          {localState?.results?.map(({ title, link, description }, index) => (
+            <div key={index} className="mb-4">
+              <a href={link} target="_blank" rel="noreferrer">
+                <p className="text-sm">
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className="text-2xl text-blue-500 hover:underline">
+                  {title}
+                </p>
+              </a>
+              <p>
+                {description.length > 30
+                  ? description.substring(0, 80) + "..."
+                  : description}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
